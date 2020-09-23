8d6ad2f31bcd6999f41f94ff6d0f997eda68d3e8
Umang A <umanga.eng@gmail.com>
2020-09-22 20:19:27 -0400

    modified for general purpose

--- oscillator_setup.c
+++ oscillator_setup.c
@@ -1,25 +1,11 @@
 /*
-Project: X1-Li Management System    ||  mvp(stage-1)
+Project: General purpose
 Author: Umang A
 Description: Check pseudo code.
+Date:
+
 */
-
-#include<stdio.h>
-//#include<pic18f45k22.h>
-
-#define True 1
-#define False 0
-
-//global variables
-
-//function prototypes
-void initialize_oscillator();
-void initialize_ports();
-void initialize_timer();
-void initiate_communication();
-void parse_sentence();
-//main function
-
+#include<p18f45k22.h>
 void initialize_oscillator(int frequency)
 {
     switch(frequency)
@@ -44,22 +30,5 @@ void initialize_oscillator(int frequency)
         OSSCON2 |= 0b00000000; // 0b xxxx xyxx ::TODO = if the module do not work properly flip the switch y which will turn the primary oscillator source on.
         OSCTUNE |= 0b00000000;
         break;
-    }
-}
-void main()
-{
-    // initialize system
-    // initialize system oscillator
-    initialize_oscillator(800);        // provide the frequency
-    initialize_ports();
-    initialize_timer();
-    initiate_communication();
-
-    while(1)
-    {
-        // check for the incoming data
-        // if msg received parse the message
-        parse_sentence();
-
     }
 }
